cbfa073d858af312debf8c8c494f58603d4ab495
Matt Giuca <mgiuca@google.com>
2012-08-28 18:41:30 +1000

    Do not divide RMS by 10000 when testing against tolerance.
    This was arbitrary and made no sense. Increased all tolerances by a factor of
    10000. Note that some are ridiculously large (e.g., 200 out of 255).

--- lib/matplotlib/testing/compare.py
+++ lib/matplotlib/testing/compare.py
@@ -308,7 +308,7 @@ def compare_images( expected, actual, tol, in_decorator=False ):
 
    diff_image = make_test_filename(actual, 'failed-diff')
 
-   if ( (rms / 10000.0) <= tol ):
+   if rms <= tol:
       if os.path.exists(diff_image):
          os.unlink(diff_image)
       return None
@@ -326,7 +326,7 @@ def compare_images( expected, actual, tol, in_decorator=False ):
    else:
       # old-style call from mplTest directory
       msg = "  Error: Image files did not match.\n"       \
-            "  RMS Value: " + str( rms / 10000.0 ) + "\n" \
+            "  RMS Value: " + str( rms ) + "\n" \
             "  Expected:\n    " + str( expected ) + "\n"  \
             "  Actual:\n    " + str( actual ) + "\n"      \
             "  Difference:\n    " + str( diff_image ) + "\n"      \
