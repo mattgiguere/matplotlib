3a83a246321356ed99f63ac6cec41c4b1829347c
Matt Giuca <mgiuca@google.com>
2012-08-28 13:44:16 +1000

    Added new test suite, test_compare_images.
    This tests the image comparison function itself. Currently, all three cases
    fail, due to a buggy comparison algorithm.
    
    In particular, test_image_compare_scrambled shows the algorithm massively
    under-computing the error, and test_image_compare_shade_difference shows the
    algorithm massively over-computing the error.

--- lib/matplotlib/__init__.py
+++ lib/matplotlib/__init__.py
@@ -1065,6 +1065,7 @@ default_test_modules = [
     'matplotlib.tests.test_cbook',
     'matplotlib.tests.test_colorbar',
     'matplotlib.tests.test_colors',
+    'matplotlib.tests.test_compare_images',
     'matplotlib.tests.test_dates',
     'matplotlib.tests.test_delaunay',
     'matplotlib.tests.test_figure',
